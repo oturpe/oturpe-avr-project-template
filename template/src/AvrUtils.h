d511993cddaf2943ea3d0fc26c953a2be6ede682
Otto Urpelainen <oturpe@iki.fi>
2019-11-02 04:07:04 +0200

    Add more AvrUtils that were needed in As above so below light controller

--- template/src/AvrUtils.h
+++ template/src/AvrUtils.h
@@ -11,6 +11,50 @@
 /// Symbols for referring to separate io ports.
 enum Port { B, C, D };
 
+/// Possible prescaler values for timers.
+enum TimerPrescalerValue {
+    PSV_1,
+    PSV_8,
+    PSV_32,
+    PSV_64,
+    PSV_128,
+    PSV_256,
+    PSV_1024,
+};
+
+/// \enum WaveformGenerationMode
+///    Possible waveform generation modes
+enum WaveformGenerationMode {
+    NORMAL,
+    PWM_PHASE_CORRECT,
+    PWM_PHASE_AND_FREQUENCY_CORRECT, // timer 1 only
+    PWM_FAST,
+    CTC
+};
+
+/// \enum CounterTop
+///    Top value for counter
+enum CounterTop {
+    TOP_00FF,
+    TOP_01FF, // timer 1 only
+    TOP_02FF, // timer 1 only
+    TOP_ICR, // timer 1 only
+    TOP_OCRA
+};
+
+/// \brief
+///    Read pin value.
+///
+/// \param port
+///    Pin port
+///
+/// \param pin
+///    Pin index
+///
+/// \return
+///    If pin is high.
+bool getData(Port port, int pin);
+
 /// \brief
 ///    Sets value of a single bith in a data register (PORTxn). Normally used
 ///    to set output pin high or low.
@@ -37,6 +81,45 @@ void setData(Port port, int pin, bool enable);
 ///
 /// \param enable
 ///    If pin is enabled (output) or disabled (input)
-void setDataDirection(Port port, int pin, bool enable);
+///
+/// \param enablePullup
+///    If pullup resistor is enabled. This value is only used when configuring
+///    an input pin.
+void setDataDirection(Port port, int pin, bool enable, bool enablePullup);
+
+/// \brief
+///    Enables pin change intterupt on the given pin.
+///
+/// \param port
+///    Pin port
+///
+/// \param pin
+///    Pin index
+void enablePinChangeInterrupt(Port port, int pin);
+
+/// Initializes timer 0 by setting waveform generation mode and prescaler.
+///
+/// This function assumes that neither Clock Select nor Waveform Generation bits
+/// have not been touched yet. Note that timer 0 does not support all
+/// Timer0PrescalerValue values. If illegal value is entered, prescaler is not
+/// set to any value.
+///
+/// Parameter top toggles using OCRA register values as the TOP for pulse
+/// width modulation. It it only used if mode is either PWM_PHASE_CORRECT or
+/// PWM_FAST.
+///
+/// \param prescalerValue
+///    Requested prescaler value
+///
+/// \param mode
+///    Waveform generation mode
+///
+/// \param top
+///    Selection of counter TOP
+void initializeTimer0(
+    TimerPrescalerValue prescalerValue,
+    WaveformGenerationMode mode,
+    CounterTop top
+);
 
 #endif //_H_OTURPE_AVR_UTILS
